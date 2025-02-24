57dae0c679e23252ac13edc662a91e1722e90362
Raheem-Baig1210 <raheembaig822@gmail.com>
2025-02-24 19:23:43 +0530

    Just

--- js/es.js
+++ js/es.js
@@ -10,3 +10,15 @@ const dynamicAdd=(...list)=>{
 }
 l1=[1,2,3]
 console.log(dynamicAdd(1,2,3,4,5,6,7,8))
+
+let a={
+    name:"Faseeh"
+}
+let b={
+    course: "FSD"
+}
+let c={
+    ...a, ...b,
+    dum:"Biryani"
+}
+console.log(c)
